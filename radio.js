8391da965fc0d6dae58aeb4edf57ca7dc55aac2a
ReliableRoseAffairs <164271757+ReliableRoseAffairs@users.noreply.github.com>
2024-06-11 01:11:44 +0100

    Add files via upload

---
+++ radio.js
@@ -0,0 +1,9 @@
+
+function redirectToinvest() {
+  // Redirect to the desired page
+  window.location.href = "invest.html";
+}
+function redirectTopersonal() {
+  // Redirect to the desired page
+  window.location.href = "personal-advisor.html";
+}
